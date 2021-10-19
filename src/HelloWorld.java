0ccfe96e6b4b56d330ca48f829cc0af4ff5d5f50
franciscomarin98 <francmarinc@gmail.com>
2021-10-19 11:39:56 -0500

    :sparkles: Learning about variables and what are the recommended rules and conventions when creating one.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,24 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Flipando en Java");
+//      Reglas para definir una variable
+//      - TipoDeDato nombreVariable;
+//      - No se puede usar ninguna palabra clave del lenguaje como nombre de variable
+//      - Los nombres de variables no pueden iniciar con un número
+//      - No se recomienda usar acentos como la letra "ñ"
+
+        int numero;
+        Integer number = 7;
+        boolean valor;
+        var variableDinamica = 15;
+        String saludar = "Hola mundo desde Java";
+
+        valor = false;
+        numero = 10;
+
+        System.out.println(saludar.toUpperCase());
+        System.out.println(numero);
+        System.out.println(number);
+        System.out.println(valor);
+        System.out.println(variableDinamica);
     }
 }
